1fdfb78f071a80ba857126ba6728c606f90a9ab2
Zandros229 <ernest.figura548@gmail.com>
2019-01-15 09:35:14 +0100

    KOL_2 poprawne

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 
 public class Main {
-
+    public static void plik(ArrayList<String> args) throws FileNotFoundException {
+        PrintWriter zapis = new PrintWriter("kol.txt");
+        for (String s:args
+             ) {
+            zapis.println(s);
+        }
+        zapis.close();
+    }
     public static void main(String[] args) throws Exception {
         ArrayList<Integer> temp=new ArrayList<Integer>();
         temp.add(4);
@@ -39,8 +48,15 @@ public class Main {
         }
         Comparator<Method> methodComparator=Comparator.comparing(Method::getReturnType, Comparator.comparing(Class::getName)).thenComparing(Method::getParameterCount);
         list.sort(methodComparator);
-        for(Method m:list){
-            System.out.println(m.getReturnType().getName()+" "+m.getName()+" "+m.getParameterCount());
+        ArrayList<String> zapis=new ArrayList<String>();
+        try {
+            for (Method m : list) {
+                System.out.println(m.getReturnType().getName() + " " + m.getName() + " " + m.getParameterCount());
+                zapis.add(m.getReturnType().getName() + " " + m.getName() + " " + m.getParameterCount());
+            }
+            plik(zapis);
+        }catch( FileNotFoundException e){
+            e.printStackTrace();
         }
 
 
